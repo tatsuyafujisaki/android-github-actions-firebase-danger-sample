60522b9258fa943b8ce5eaa34b1e3c8ab04a3842
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-27 17:36:48 +0900

    Delete danger-mini.yaml and more

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,3 @@
-// Ignore inline messages which lay outside a diff's range of PR
-github.dismiss_out_of_range_messages
-
 import { schedule } from 'danger'
 
 const ktlintReport = require("danger-plugin-ktlint-report")
