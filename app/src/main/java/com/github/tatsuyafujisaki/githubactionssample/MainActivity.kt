18707e1ad6357a9b4eb28fa7762c686503686ea2
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-27 18:17:52 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
@@ -8,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    
+    val aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
