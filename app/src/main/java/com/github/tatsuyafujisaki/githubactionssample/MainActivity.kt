bb0d4f6c867c49ff32db97abd1a21b8b049e7c65
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-08-27 19:46:27 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
@@ -22,8 +22,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
     
-    val bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: String
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
